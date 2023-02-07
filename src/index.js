7a84e04885be58c41ed47c1c3a94f317c9b3184b
Avgchamp <gotovchit.99@gmail.com>
2023-02-07 17:54:22 +0300

    comit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import * as ReactDOMClient from 'react-dom/client';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <App />
-);
+const element = (
+    <div className='remboooo'>
+        <h1>Yaaaaaaaa</h1>
+    </div>
+)
+
+const app = ReactDOMClient.createRoot(document.getElementById("root"));
+
+app.render(element);
