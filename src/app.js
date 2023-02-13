2ef48e73be1a2bd889d3baa474d7aea039021819
Avgchamp <gotovchit.99@gmail.com>
2023-02-13 12:28:37 +0300

    commit

--- src/app.js
+++ src/app.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import Header from './components/Header';
 import './css/main.css';
-import Users from './components/Users';
 
 
 class App extends React.Component{
@@ -9,15 +7,10 @@ class App extends React.Component{
     render(){
         return(
             <div>
-                <Header title="Список пользователей" />
-                <main>
-                    <Users />
-                </main>
-                <aside></aside>
+                
             </div>
         )
     }
-
 }
 
 export default App;
