eee8830724abcfca916952203bca4b2b92882c1d
Avgchamp <gotovchit.99@gmail.com>
2023-02-16 17:45:43 +0300

    cum

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import './css/main.css';
+import Card from './components/Card';
+import Footer from './components/Footer';
+import Main from './components/Main';
 
 
 class App extends React.Component{
@@ -7,7 +10,9 @@ class App extends React.Component{
     render(){
         return(
             <div>
-                
+                <Card />
+                <Main />
+                <Footer />
             </div>
         )
     }
---
+++ src/components/Card.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class Card extends React.Component{
+    
+    render(){
+        return(
+            <div className='Card'>
+
+                <div className='headerCard'>
+                    Header
+                </div>
+
+                <div className='mainCard'>
+                    MainNav
+                </div>
+
+            </div>
+        )
+    }
+}
+
+export default Card;
