faa5692575949abadf0552ae84d3058db374760f
Avgchamp <gotovchit.99@gmail.com>
2023-03-07 17:06:46 +0300

    com

--- src/app.js
+++ src/app.js
@@ -1,21 +1,27 @@
-import React from 'react';
-import './css/main.css';
-import Card from './components/Card';
-import Footer from './components/Footer';
-import Main from './components/Main';
+import React from "react";
+import "./css/main.css";
+import Card from "./components/Card";
+import Footer from "./components/Footer";
+import Main from "./components/Main";
 
-
-class App extends React.Component{
-    
-    render(){
-        return(
-            <div>
-                <Card />
-                <Main />
-                <Footer />
-            </div>
-        )
-    }
+class App extends React.Component {
+  render() {
+    return (
+      <div class="container">
+        <div class="row row-cols-3">
+          <div class="col">Column</div>
+          <div class="col">Column</div>
+          <div class="col">Column</div>
+          <div class="col">Column</div>
+          <div class="col">Column</div>
+          <div class="col">Column</div>
+        </div>
+        <div class="row">
+          <div class="col" class="center-block">col-6</div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import * as ReactDOMClient from 'react-dom/client';
 import App from './app';
-
+import 'bootstrap/dist/css/bootstrap.css';
 
 const app = ReactDOMClient.createRoot(document.getElementById("root"));
 
